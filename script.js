11ea6b14c41cdfacf42c72f4a5108413ce65a433
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-02-14 23:52:40 -0600

    make array, book constructor, book maker function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//
+const library = [];
+
+//constructor for books
+function Book(title,author,pages,readYet) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readYet = readYet;
+}
+
+//separate function that takes arguments
+function addBookToLibrary(title,author,pages,readYet) {
+    //makes a book from arguments
+    let book = new Book(title,author,pages,readYet);
+    //each book object needs a unique id generated using crypto.randomUUID() to prevent issues when
+        //books are removed or rearranged
+    book.id = crypto.randomUUID();
+    //stores new book object into array
+    library.push(book);
+}
