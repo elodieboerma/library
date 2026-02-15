f2e190836343134865e4f2557252f44543552ed6
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-02-15 00:25:02 -0600

    visually separate books

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ addBookToLibrary("The Hobbit","J.R.R. Tolkien",295,"not read yet");
 addBookToLibrary("Emma","Jane Austen",208,"read");
 addBookToLibrary("Anna Karenina","Leo Tolstoy",880,"not read yet");
 
-//constructor for books
+// main
+
 function Book(title,author,pages,readYet) {
     this.title = title;
     this.author = author;
@@ -12,21 +13,16 @@ function Book(title,author,pages,readYet) {
     this.readYet = readYet;
 }
 
-//separate function that takes arguments
 function addBookToLibrary(title,author,pages,readYet) {
-    //makes a book from arguments
     let book = new Book(title,author,pages,readYet);
-    //each book object needs a unique id generated using crypto.randomUUID() to prevent issues when
-        //books are removed or rearranged
     book.id = crypto.randomUUID();
-    //stores new book object into array
     library.push(book);
 }
 
-//function that loops through array and displays each book on the page
 for(const book of library) {
     const bookshelf = document.getElementById("bookshelf");
     const newBook = document.createElement("div");
     newBook.textContent = `${book.title} (${book.author})`;
+    newBook.classList.add("bookDecoration");
     bookshelf.append(newBook);
 }
